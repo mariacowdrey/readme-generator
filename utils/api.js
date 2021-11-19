cb8a0b8d6a0b77426738a80175ee0368a0022e7c
Maria Cowdrey <mariacowdrey@yahoo.com>
2021-11-18 19:46:29 -0800

    add question validation and generate file

--- utils/api.js
+++ utils/api.js
@@ -1,13 +1,16 @@
 const axios = require('axios');
 
 const api = {
-    async getUser(userResponses) {
-      let response = await axios
+  async getUser(userResponses) {
+    try { let response = await axios
+        
         .get(`https://api.github.com/users/${userResponses.username}`);
         return response.data;
-      }
-  };
 
-  module.exports = {
-    apiCalls: api
-  };
+      } catch (error) {
+        console.log(error);
+      }
+  }
+};
+
+module.exports = api;
