2e6d85de053140911e49f9de40d72ecb0d460d4c
Maria Cowdrey <mariacowdrey@yahoo.com>
2021-11-18 21:09:45 -0800

    updated questions

--- index.js
+++ index.js
@@ -55,28 +55,36 @@ const questions = [
     },
     {
         type: 'input',
-        message: "If applicable, describe the steps required to install your project for the Installation section.",
+        message: "If applicable, describe the steps required to install your project.",
         name: 'installation'
     },
     {
         type: 'input',
-        message: "Provide instructions and examples of your project in use for the Usage section.",
+        message: "How do you use it?",
         name: 'usage'
     },
     {
         type: 'input',
-        message: "If applicable, provide guidelines on how other developers can contribute to your project.",
+        message: "If applicable, how can other developers contribute to your project?",
         name: 'contributing'
     },
     {
         type: 'input',
-        message: "If applicable, provide any tests written for your application and provide examples on how to run them.",
+        message: "If applicable, provide any tests written for your application and how to run them.",
         name: 'tests'
     },
     {
         type: 'list',
         message: "Choose a license for your project.",
-        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        choices: [
+            "Apache",
+            "Academic",
+            "GNU",
+            "ISC",
+            "MIT",
+            "Mozilla",
+            "Open"
+        ],
         name: 'license'
     }
 ];
